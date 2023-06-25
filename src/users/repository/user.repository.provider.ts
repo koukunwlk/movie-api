4b9bb6e0a2019368ecccda9f074bff8a4ab4c27f
Moa Amaro <moacir.amaro@mindlab.com.br>
2023-06-25 18:30:23 -0300

    chore: rename folder users to user

--- src/users/repository/user.repository.provider.ts
+++
@@ -1,8 +0,0 @@
-import { ClassProvider } from '@nestjs/common';
-import { MongoDBUserRepository } from './mongodb/mongodb-user.repository';
-import { UserRepository } from './user.repository';
-
-export const UserRepositoryProvider: ClassProvider = {
-  provide: UserRepository,
-  useClass: MongoDBUserRepository,
-};
--- src/users/repository/user.repository.ts
+++
@@ -1,6 +0,0 @@
-import { User, UserInterface } from '../domain/models/user.model';
-
-export abstract class UserRepository {
-  abstract create: (user: User) => Promise<void>;
-  abstract findOne: (options: Partial<UserInterface>) => Promise<User | null>;
-}
